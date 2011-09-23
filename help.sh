6b635d38cbc7124e6d5ee02ad89e7f5447863165
2085020 <joan79@gmail.com>
2011-09-23 16:18:33 +0200

    ayuda Git v2

--- help.sh
+++ help.sh
@@ -5,4 +5,9 @@ git add files
 git commit -m "Comment"
 git remote add origin git@github.com:2085020/GitHub-Help.git
 git push -u origin master
+//añadir cambios del trunk a tu fork
+git remote add "upstream" "url_repo_original"
+git pull upstream branch
 
+//merge
+git fetch
